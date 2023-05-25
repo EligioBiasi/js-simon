4ab987edfe0960645037f51379f9a63eeeb0d0c8
EligioBiasi <biasi.eligio@edufaimm.it>
2023-05-25 16:47:51 +0200

    timer finished

---
+++ script.js
@@ -0,0 +1,30 @@
+const countDown = document.querySelector('#countdown')
+let dateOfArrival = new Date('May 26, 2023 09:30:00').getTime();
+
+let counter = setInterval(function(){
+
+    const today = new Date().getTime();
+    
+    let timeDifference = dateOfArrival-today;
+
+    if(timer=0){
+        clearInterval(dateOfArrival);
+        document.getElementById('countdown').innerHTML='Inizia la lezione'
+    }else{
+
+
+        let second = Math.floor(timeDifference/1000);
+
+        let minute = Math.floor(second/60);
+
+        let hour = Math.floor(minute/60);
+
+        countDown.innerHTML = `${hour} ore ${minute % 60} minuti ${second % 60} secondi`;
+        console.log(hour + ' ore ' + (minute % 60) +  ' minuti '+ (second % 60) + ' secondi ');
+
+
+    }
+
+    
+
+}, 1000);
